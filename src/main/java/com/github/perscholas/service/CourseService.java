a6de6a87364b741c05032c61c0c8cd96853c49ae
Susan <sttabassum93@gmail.com>
2020-02-19 13:24:51 -0500

    Completed CourseService

--- src/main/java/com/github/perscholas/service/CourseService.java
+++ src/main/java/com/github/perscholas/service/CourseService.java
@@ -1,14 +1,37 @@
 package com.github.perscholas.service;
 
+import com.github.perscholas.DatabaseConnection;
 import com.github.perscholas.dao.CourseDao;
+import com.github.perscholas.model.Course;
 import com.github.perscholas.model.CourseInterface;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 // TODO - Implement respective DAO interface
 public class CourseService implements CourseDao {
+    private final DatabaseConnection dbc;
+
+    public CourseService(DatabaseConnection dbc) {this.dbc = dbc;}
+
+    public CourseService() {this(DatabaseConnection.MARIADB);}
+
     @Override
-    public List<CourseInterface> getAllCourses() {
-        return null;
+    public List<CourseInterface> getAllCourses() throws SQLException {
+        ResultSet result = dbc.executeQuery("SELECT * FROM `course`");
+        List<CourseInterface> list = new ArrayList<>();
+        try {
+            while (result.next()) {
+                Integer id = result.getInt("id");
+                String name = result.getString("name");
+                String instructor = result.getString("instructor");
+                CourseInterface course = new Course(id, name, instructor);
+            }
+        } catch (SQLException se) {
+            throw new Error(se);
+        }
+        return list;
     }
 }
